1455584e6ee659e5c611638251b6a130b76b565b
Endimionx <endimion7@gmail.com>
2017-05-17 04:15:47 +0700

    Initial Commit

---
+++ Target Support Files/NYAlertViewController/NYAlertViewController-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "NYAlertView.h"
+#import "NYAlertViewController.h"
+
+FOUNDATION_EXPORT double NYAlertViewControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char NYAlertViewControllerVersionString[];
+
